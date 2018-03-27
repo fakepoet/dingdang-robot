ea9e4807964829afc8ab2df9c69262fc65c2a431
fakepoet <fakepoet0101@gmail.com>
2018-03-28 01:38:39 +0800

    raise up CHUNK size

--- client/mic.py
+++ client/mic.py
@@ -129,7 +129,7 @@ class Mic:
 
         THRESHOLD_MULTIPLIER = 2.5
         RATE = 16000
-        CHUNK = 1024
+        CHUNK = 4096
 
         # number of seconds to allow to establish threshold
         THRESHOLD_TIME = 1
